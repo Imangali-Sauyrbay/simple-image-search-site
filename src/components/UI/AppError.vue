95f712feb5255618ccef3a7facfe9ea5710f639c
S.Iman <simangali2002@gmail.com>
2023-09-23 12:46:10 +0600

    style: app error now drops shadow

--- src/components/UI/AppError.vue
+++ src/components/UI/AppError.vue
@@ -1,6 +1,6 @@
 <template>
-  <h1 class="text-center text-lg font-bold text-red-600">
-    Ooops... It seems there is an error.<br />It could be unsplash api requests limitting, so please
-    try again later!
+  <h1 class="my-4 text-center text-lg font-bold text-red-700 drop-shadow-md">
+    Ooops... It seems there is an error.<br />It could be unsplash api requests limitting, so please,
+    try again later, approximately... next hour!
   </h1>
 </template>
